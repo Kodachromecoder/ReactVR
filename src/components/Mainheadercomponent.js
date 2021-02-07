3ab16bc56bf351e6bf7e2f309f84b9ec0f060f1b
Kara Kelly <ksquared9595@gmail.com>
2021-02-06 18:40:27 -0600

    Main Components set up

---
+++ src/components/Mainheadercomponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Mainheader extends Component {
+    render() {
+        return (
+            <div className="purplebox">
+                <h1>I'll be your main header for the home page only. I'll have your logo and a toggle button for your nav bar. I'll have a cool image that is unique to the home page.</h1>
+            </div>
+        );
+    }
+}
+
+export default Mainheader;
---
+++ src/components/Secondaryheader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Secondaryheader extends Component {
+    render() {
+        return (
+            <div className="secondheader">
+                <h1>I'll be your secondary header for the rest of the website. I'll have your logo and a toggle button for your nav bar. I'll have a cool image or maybe just a color banner?</h1>
+            </div>
+        );
+    }
+}
+
+export default Secondaryheader;
